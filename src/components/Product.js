1d9f039c32b2d8bca5a98dd7d8e87bd74a2f7ce2
Jason Sears <jsears4585@gmail.com>
2017-07-17 14:56:55 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,71 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
 // Code Product Component Here
+class Product extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <h1>{this.props.producer}</h1>
+        <h1>{this.props.hasWatermark}</h1>
+        <h1>{this.props.color}</h1>
+        <h1>{this.props.weight}</h1>
+      </div>
+    )
+  }
+}
+
+function productWeightChecker(props, propName, componentName) {
+  if (props[propName] > 300 || props[propName] < 80) {
+    return new Error('Validation failed!')
+  } else if (typeof props[propName] !== 'number') {
+    return new Error('Validation failed again!')
+  } else {
+    return null
+  }
+  let chainedCheckRange = productWeight.bind(null, false)
+  chainedCheckRange.isRequired = productWeight.bind(null, true)
+  return chainedCheckRange
+}
+
+function createChainableTypeChecker(validate) {
+  function checkType(isRequired, props, propName, componentName, location) {
+    if (props[propName] == null) {
+      var locationName = ReactPropTypeLocationNames[location];
+      if (isRequired) {
+        return new Error(
+          ("Required " + locationName + " `" + propName + "` was not specified in ") +
+          ("`" + componentName + "`.")
+        );
+      }
+      return null;
+    } else {
+      return validate(props, propName, componentName, location);
+    }
+  }
+
+  var chainedCheckType = checkType.bind(null, false);
+  chainedCheckType.isRequired = checkType.bind(null, true);
+
+  return chainedCheckType;
+}
+
+let productWeight = createChainableTypeChecker(productWeightChecker)
+
+Product.defaultProps= {
+  hasWatermark: false
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white','eggshell-white','salmon']).isRequired,
+  weight: productWeight
+}
+
+export default Product
